4207d911e87020b70e58a6b01ad9521010aa460f
Waseem Alyazidi <waseem.alyazidi18@gmail.com>
2025-09-11 00:00:27 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -53,6 +53,9 @@ from utils import calculate_bmi, data_encoding # type: ignore
 app = Flask(__name__, template_folder=r"../frontend/templates", static_folder=r"../frontend/statics")
 
 MODEL_PATH = Path(__file__).resolve().parent / "model.pkl"
+
+globals()['np'] = np
+
 try:
     # Load the model
     with open(MODEL_PATH, mode="rb") as f:
